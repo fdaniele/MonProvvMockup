15ff81843b6335bfa685f155ae097c6d1025986e
d.fiorio <d.fiorio@169.254.133.69>
2014-09-19 15:22:47 +0200

    Aggiunto modal inserimento note/allegati

--- MonProvvMockup/WebRoot/js/scripts.js
+++ MonProvvMockup/WebRoot/js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-//    $('.multiselect').multiselect({
-//            nonSelectedText: 'Tutti',
-//            numberDisplayed: 20
-//	});
+    $('.multiselect').multiselect({
+            nonSelectedText: 'Tutti',
+            numberDisplayed: 20
+	});
     
     
     var btnRicAvDown = $("#btnRicAvDown");
@@ -56,7 +56,22 @@ $(document).ready(function() {
     $("#deleteEnte").click(function() {
     	//assegnatarioDipTesoro.show();
     	assegnatarioDipTesoro.fadeOut( 1000 );
-    });    
+    });
+    
+    $('#modal').on('show', function () {
+        $(this).find('.modal-body').css({
+               width:'auto', //probably not needed
+               height:'auto', //probably not needed 
+               'max-height':'100%'
+        });
+    });
+    
+    $("#popoverNotifiche").popover({
+    	placement : 'bottom', // top, bottom, left or right
+    	title : 'Notifiche&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**', 
+    	html: 'true', 
+    	content : '<div id="popOverBox"><span>L\'utente Filippo Neri ha richiesto l\'assegnazione del provvedimento LP 1 per l\'organo Ragioneria Generale dello Stato</span><br><a href="richiestaassegnazione.html">Clicca qui</a> per gestire la richiesta<hr/><span>L\'utente Carlo Bianchi ha rifiutato l\'assegnazione del provvedimento LP 1 per l\'organo Dip.to Tesoro</span><br><a href="motivazionerifiuto.html">Clicca qui</a> per vedere la motivazione</div>'
+    });
 });
 
 
