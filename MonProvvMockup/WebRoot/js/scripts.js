7265daaee71e12a416e5bee0cf58f9a32a55975f
francesco.carlucci <francarl@gmail.com>
2014-09-19 16:42:33 +0200

    Modifiche ricerca, inserimento e dettaglio, pagine di rifiuto e richiesta assegnazione

--- MonProvvMockup/WebRoot/js/scripts.js
+++ MonProvvMockup/WebRoot/js/scripts.js
@@ -18,8 +18,8 @@ $(document).ready(function() {
   	  btnRicAvUp.show();
     });
     
-    $("#annulla").click(function() {
-        location.reload();
+    $("#annulla, #salva").click(function() {
+    	window.location.href = "index.html";
     });
     
     $("#indietro").click(function() {
@@ -38,7 +38,7 @@ $(document).ready(function() {
     });
     
     
-    $('.table > tbody > tr').click(function() {
+    $('#risultatiRicerca .table > tbody > tr').click(function() {
     	window.location.href = "dettaglio.html";
     });
     
@@ -51,19 +51,61 @@ $(document).ready(function() {
     $("#insertEnte").click(function() {
     	//assegnatarioDipTesoro.show();
     	assegnatarioDipTesoro.fadeIn( 1000 );
+    	$("#noRecord").hide();
     });
     
-    $("#deleteEnte").click(function() {
+    $("#deleteEnte").click(function(e) {
     	//assegnatarioDipTesoro.show();
     	assegnatarioDipTesoro.fadeOut( 1000 );
+    	$("#noRecord").show();
+    	e.preventDefault();
     });
     
+    
+    $('#popoverRifiuto').on('click', function(e) {e.preventDefault(); return true;});
+    
+    var $div = $('<div>');
+    var content = 'ciao';
+    $div.load('motivazionerifiuto.html #modalRifiuto', function() {
+    	content = $(this).html();
+    });
+
+    $('html').click(function(e) {
+        $('#popoverRifiuto').popover('hide');
+    });
+    $("#popoverRifiuto").click( function(e) {  
+    	$(this).popover({
+	    	placement : 'top', // top, bottom, left or right
+	    	title : 'Rifiuto assegnazione', 
+	    	html: 'true',
+	    	trigger: 'manual',
+	    	content : content // '<div id="popOverBox"><span>Provvedimento non di competenza<span></div>'
+    	}); 
+    	$(this).popover('toggle');
+    	e.stopPropagation();
+    });
+    
+    
+    
+    // NOTIFICHE
     $("#popoverNotifiche").popover({
     	placement : 'bottom', // top, bottom, left or right
     	title : 'Notifiche&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**', 
     	html: 'true', 
     	content : '<div id="popOverBox"><span>L\'utente Filippo Neri ha richiesto l\'assegnazione del provvedimento LP 1 per l\'organo Ragioneria Generale dello Stato</span><br><a href="richiestaassegnazione.html">Clicca qui</a> per gestire la richiesta<hr/><span>L\'utente Carlo Bianchi ha rifiutato l\'assegnazione del provvedimento LP 1 per l\'organo Dip.to Tesoro</span><br><a href="motivazionerifiuto.html">Clicca qui</a> per vedere la motivazione</div>'
     });
+
+    
+    
+    // INSERIMENTO
+    $("#dp1").datepicker({
+    	format: "dd/mm/yyyy",
+        todayBtn: "linked",
+        language: "it"
+    }).on('changeDate', function(ev){
+	        $("#dp1v").val(ev.format('dd/mm/yyyy'));
+        });
+  
 });
 
 
